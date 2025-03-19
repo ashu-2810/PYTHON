a1b67287fb4d93bd94f0de685b0ad9dd29f04817
Ashu <ashuprince7337@gmail.com>
2025-03-19 15:11:58 +0530

    update

---
+++ LERN-PYTHON/Day01.py
@@ -0,0 +1,11 @@
+#  type of variables-> int,str,float,boolean(True,False),none
+# keyword -> and,as,assert,break,class,continue,ddef,del,elif,else,expect,finally,False,for,from,global,if,import,in,is,lambda,nonlocal,None,not,oor,pass,raise,return,True,try,with,while,yield
+# python is a case sensitive eg;- A & a are different
+# Q1 print Sum
+a=2
+b=5
+sum=a+b
+print("sum =",sum)
+
+# type of tocken
+# punctuators -> -=,+=,/=,==,=,etc.
